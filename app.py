aad97eedd9bf19dec7236372a8ed31be5fcea8cd
echocraft201315 <sachkhiha33@gmail.com>
2025-04-06 23:17:10 -0700

    initial

---
+++ app.py
@@ -0,0 +1,70 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import warnings
+
+plt.rc('figure', autolayout=True)
+plt.rc('axes', labelweight='bold', labelsize='large',
+       titleweight='bold', titlesize=18, titlepad=10)
+plt.rc('image', cmap='magma')
+warnings.filterwarnings("ignore") # to clean up output cells
+
+# Read image
+image_path = './34.jpg'
+image = tf.io.read_file(image_path)
+image = tf.io.decode_jpeg(image, channels = 1)
+
+# plt.figure(figsize=(6, 6))
+# plt.imshow(tf.squeeze(image), cmap='gray')
+# plt.axis('off')
+# plt.show()
+
+#define kernel
+kernel = tf.constant([
+    [-1, -1, -1],
+    [-1,  8, -1],
+    [-1, -1, -1],
+])
+
+# Reformat for batch compatibility.
+image = tf.image.convert_image_dtype(image, dtype=tf.float32)
+image = tf.expand_dims(image, axis=0)
+kernel = tf.reshape(kernel, [*kernel.shape, 1, 1])
+kernel = tf.cast(kernel, dtype=tf.float32)
+
+#filter step
+image_filter = tf.nn.conv2d(
+    input=image,
+    filters=kernel,
+    # we'll talk about these two in lesson 4!
+    strides=1,
+    padding='SAME',
+)
+
+# plt.figure(figsize=(6, 6))
+# plt.imshow(tf.squeeze(image_filter))
+# plt.axis('off')
+# plt.show()
+
+#image detect
+image_detect = tf.nn.relu(image_filter)
+
+plt.figure(figsize=(6, 6))
+plt.imshow(tf.squeeze(image_detect))
+plt.axis('off')
+plt.show()
+
+import tensorflow as tf
+
+image_condense = tf.nn.pool(
+    input=image_detect, # image in the Detect step above
+    window_shape=(2, 2),
+    pooling_type='MAX',
+    # we'll see what these do in the next lesson!
+    strides=(2, 2),
+    padding='SAME',
+)
+
+plt.figure(figsize=(6, 6))
+plt.imshow(tf.squeeze(image_condense))
+plt.axis('off')
+plt.show()
